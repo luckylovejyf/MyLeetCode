c21f7b69d9141bedf682ca49897e3154151dc5c4
luckylovejyf <luckylove2022@163.com>
2023-01-27 20:18:22 +0800

    feat: 95

--- dp/95.go
+++ dp/95.go
@@ -7,6 +7,29 @@ type TreeNode struct {
 }
 
 func generateTrees(n int) []*TreeNode {
+	if n <= 0 {
+		return []*TreeNode{nil}
+	}
 
-	return nil
+	return helper(1, n)
+}
+
+func helper(start int, end int) []*TreeNode {
+	if start > end {
+		return []*TreeNode{nil}
+	}
+
+	res := make([]*TreeNode, 0)
+	for i := start; i <= end; i++ {
+		left := helper(start, i-1)
+		right := helper(i+1, end)
+
+		for _, leftNode := range left {
+			for _, rightNode := range right {
+				res = append(res, &TreeNode{i, leftNode, rightNode})
+			}
+		}
+	}
+
+	return res
 }
