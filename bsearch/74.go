7c7dade91bc8279be38bd92e20959b5cf33d58a0
luckylovejyf <luckylove2022@163.com>
2023-02-12 15:22:22 +0800

    feat: 74

--- bsearch/74.go
+++ bsearch/74.go
@@ -1,5 +1,10 @@
 package bsearch
 
+// 74. 搜索二维矩阵
+// 首先二分查找第一列，找到<=target的一行，然后二分查找这一行
+// 二分查找<=：l=-1, r=len-1; mid=(r-l+1)/2+l; l=mid, r= mid-1 最后要判断l<0
+// 二分查找==；l=0, r=len-1; mid=(r-l)/2+l; l=mid+1, r=mid-1
+
 func searchMatrix(matrix [][]int, target int) bool {
 	if len(matrix) == 0 {
 		return false
