3b641db29089afab8a116195fe2c51b3312372be
luckylovejyf <luckylove2022@163.com>
2023-03-19 22:09:21 +0800

    feat: 64

---
+++ hot100/64.go
@@ -0,0 +1,32 @@
+package hot100
+
+// 64. 最小路径和 动态规划
+// 只能向下或者向右走，所以到达每一个节点的最小路径可以由左节点和右节点计算得来
+// 初始化所有边界的长度为前序累加
+func minPathSum(grid [][]int) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+
+	m := len(grid)
+	n := len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + grid[0][i]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+
+	return dp[m-1][n-1]
+}
